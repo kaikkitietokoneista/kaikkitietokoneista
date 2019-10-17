b22a2b720bec6b2988d8aabd7721c32dab339599
Kaikkitietokoneista.net <52327670+kaikkitietokoneista@users.noreply.github.com>
2019-10-17 12:29:48 +0300

    Create botit.php

---
+++ botit.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Etusivu &laquo; kaikkitietokoneista.net</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="main.css">
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+    crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <header style="padding: 16px; background-image: url('https://cdn.pixabay.com/photo/2017/05/29/18/22/matrix-2354492_960_720.jpg'); /*linear-gradient(#939de6, white)*/">
+  		<h1 style="color: white;">Kaikkitietokoneista.<wbr>net</h1>
+  		<a href="/" class="nappi">Etusivu</a>
+  		<a href="?q=verkko" class="nappi">Lähiverkon rakentaminen</a>
+  		<a href="" class="nappi">Ohjeet</a>
+      <div class="alasveto">
+  		    <a href="ohjelmat" class="nappi">Ohjelmat</a>
+          <div class="alasvetolaatikko">
+            <a href="ohjelmat" class="nappi">Ohjelmat</a>
+            <a href="http://kaikkitietokoneista.net/8080" class="nappi">Työkalut</a>
+            <a href="botit" class="nappi">Botit</a>
+          </div>
+      </div>
+  		<div class="oikea"><script>
+        var d = new Date();
+        var months = ["Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"]
+        document.write(months[d.getMonth()], " ", d.getDate(), " ", d.getHours(), ":", d.getMinutes());
+      </script>
+      </div>
+  	</header>
+    <div class="oikealle neljäsosa"><a class="twitter-timeline" href="https://twitter.com/RoyTakanen?ref_src=twsrc%5Etfw">RoyTakasen Tweetit</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></div>
+    <h4 class="p16">Sääbotti (Node.JS)</h4>
+    <p class="p16">Botti ilmoittaa sinulle Telegramissa yksinkertaisella komennolla annetun kaupungin sään. Voit tavoittaa botin osoitteesta <a href="https://t.me/ktweather_bot">t.me/ktweather_bot</a>. Botin lähdekoodi on avoin ja löydät sen <a href="https://github.com/kaikkitietokoneista/weather-tgbot">Githubista</a>. Tehty Node.JS:llä</p>
+    <footer style="background-color: #16324F; color: white;" class="p16">
+      <center><p>Hakkereille, ohjelmoijille, tietokonenörteille ja valkohatuille suunnattu sivusto.</p></center>
+      <p class="p16"><b>Muista:</b> näitä ohjeita ei ole tarkoitettu pahaan, jotenka jos pystyisit esimerkiksi murtamaan koulusi sisäverkon näillä ohjeilla. Niin sinun tulee ilmoittaa siitä ATK-henkilöstölle eikä kerätä koevastauksia avoimien printtereiden kautta.</p>
+    </footer>
+  </body>
+</html>
