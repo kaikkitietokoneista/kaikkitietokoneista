3b61bfb8c94015731ca714719b28f6cd4d94136b
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2019-10-29 18:49:10 +0200

    Update ohjelmat.php

--- ohjelmat.php
+++ ohjelmat.php
@@ -23,9 +23,15 @@
     <h4 class="p16">Proxymoxy (PHP)</h4>
     <p class="p16">Yhden tiedoston ohjelma, joka toimii niin sanottuna eliittiproxyna. Voit esiintyä ilman erillisten proxyien asentelua eri IP-osoitteella. Ohjelman lähdekoodi on avoin ja löydät sen <a href="https://github.com/kaikkitietokoneista/proxymoxy">Githubista</a>. Tehty PHP:lla.</p>
 
-    <h4 class="p16">Proxymoxy (JavaScript)</h4>
+    <h4 class="p16">JSEdit (JavaScript)</h4>
     <p class="p16">Yhden tiedoston ohjelma (teen vain yhden tiedoston ohjelmia), joka toimii WYSIWYG-editorina selaimessa. Ohjelman lähdekoodi on avoin ja löydät sen <a href="https://github.com/kaikkitietokoneista/jsedit">Githubista</a>. Tehty JavaScript:lla.</p>
     <p class="p16">Demo: <a href="https://kaikkitietokoneista.github.io/jsedit/">kaikkitietokoneista.github.io/jsedit/</a></p>
+
+    <h4 class="p16">JS visual (JavaScript)</h4>
+    <p class="p16">JavaScript ohjelmointiympäristö, jossa ohjelmoidaan palikoilla. Perustuu <a href="https://developers.google.com/blockly">Google Blocklyyn</a>. Ohjelman lähdekoodi on avoin ja löydät sen <a href="https://github.com/kaikkitietokoneista/JS-visual">Githubista</a>. Tehty JavaScript:lla.</p>
+    <p class="p16">Demo: <a href="https://kaikkitietokoneista.github.io/JS-visual/">https://kaikkitietokoneista.github.io/JS-visual/</a></p>
+
+
 
     <?php include 'footer.php'; ?>
   </body>
