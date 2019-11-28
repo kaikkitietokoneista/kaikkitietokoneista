73ab44687362cf1c377a40a8411fcd03bbbf3e4b
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2019-11-28 22:34:58 +0200

    Lisätty hinnasto

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <script>
 //Rewrites http to https
-if (location.protocol == "http:") {
+/*if (location.protocol == "http:") {
   console.log('changing protocol to https');
   location.protocol = "https:";
-} 
+} */
 </script>
 <header style="padding: 16px; background-image: url('src/header.jpg'); /*linear-gradient(#939de6, white)*/">
   <link rel="icon" href="src/con.png">
--- hostaus.php
+++ hostaus.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fi" dir="ltr">
+  <!-- Tee mobiiliresponsiivisuus -->
   <head>
     <meta charset="utf-8">
     <title>Hostaus &laquo; kaikkitietokoneista.net</title>
@@ -18,26 +19,85 @@
     <div class="neljäsosa oikealle">
       <iframe src="twitterembed.html" frameBorder="0" width="100%" height="500px"></iframe>
     </div>
-    <h3>Hostaus</h3>
-    <i>Enemmän kuin web-hotelleja</i>
-    <h4>Web-hotellit</h4>
-    <table>
-      <tr>
-        <th></th>
-        <th>Ilmainen</th>
-        <th>Pieni</th>
-      </tr>
-      <tr>
-        <td>Hinta: </td>
-        <td>0 €</td>
-        <td>2 €</td>
-      </tr>
-      <tr>
-        <th>Levytila</th>
-        <th>10 MB</th>
-        <th>100 MB</th>
-      </tr>
-    </table>
+    <div class="p16">
+      <h2>Hostaus</h2>
+      <i>Enemmän kuin web-hotelleja</i>
+      <h3>Domainit</h3>
+      <p>Tarjoan fi-päätteisiä domaineja. Olen Ficoran valtuuttama fi-verkkotunnusvälittäjä. Ota yhteyttä Telegramin (saat sähköpostitse tietää nimen) tai <a href="mailto:info@kaikkitietokoneista.net?subject=Haluaisin%20varata%20domainin">sähköpostin</a> kautta.</p>
+      <h3>Web-hotellit</h3>
+      <table>
+        <tr>
+          <th>Paketti:</th>
+          <th>Ilmainen</th>
+          <th>Pieni</th>
+        </tr>
+        <tr>
+          <td>Hinta: </td>
+          <td>0 €</td>
+          <td>2 €</td>
+        </tr>
+        <tr>
+          <td>Levytila:</td>
+          <td>10 MB</td>
+          <td>100 MB</td>
+        </tr>
+        <tr>
+          <td>Kaistanleveys/kk:</td>
+          <td>10 000 MB</td>
+          <td>100 000 MB</td>
+        </tr>
+        <tr>
+          <td>PHP:</td>
+          <td>PHP-FPM</td>
+          <td>PHP-FPM</td>
+        </tr>
+        <tr>
+          <td>SSL:</td>
+          <td>&#x2713; (Let's Encrypt)</td>
+          <td>&#x2713; (Let's Encrypt)</td>
+        </tr>
+        <tr>
+          <td>FTP-käyttäjiä:</td>
+          <td>2</td>
+          <td>10</td>
+        </tr>
+        <tr>
+          <td>Shell-käyttäjiä:</td>
+          <td>1</td>
+          <td>5</td>
+        </tr>
+        <tr>
+          <td>Web domaineja:</td>
+          <td>1</td>
+          <td>5</td>
+        </tr>
+        <tr>
+          <td>Alidomaineja:</td>
+          <td>1</td>
+          <td>5</td>
+        </tr>
+        <tr>
+          <td>Aliasdomaineja:</td>
+          <td>1</td>
+          <td>5</td>
+        </tr>
+        <tr>
+          <td>Hallintapaneeli:</td>
+          <td>&#x2713;</td>
+          <td>&#x2713;</td>
+        </tr>
+        <tr>
+          <td>Python:</td>
+          <td></td>
+          <td>&#x2713;</td>
+        </tr>
+        <tr>
+          <td>Varmuuskopionti:</td>
+          <td></td>
+          <td>&#x2713;</td>
+        </tr>
+      </table>
+    </div>
     <?php include 'footer.php'; ?>
   </body>
 </html>
