13530d8f9a8d64de72250cbb778e67ac6ac71e81
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2019-12-14 20:55:48 +0200

    Aloitetaan alusta

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer style="background-color: #16324F; color: white;" class="p16">
-  <p class="p16">IP-osoitteesi on: <?php echo $_SERVER["REMOTE_ADDR"]; ?></p>
-  <center><p>Hakkereille, ohjelmoijille, tietokonenörteille ja valkohatuille suunnattu sivusto.</p></center>
-  <p class="p16"><b>Muista:</b> näitä ohjeita ei ole tarkoitettu pahaan, jotenka jos pystyisit esimerkiksi murtamaan koulusi sisäverkon näillä ohjeilla. Niin sinun tulee ilmoittaa siitä ATK-henkilöstölle eikä kerätä koevastauksia avoimien printtereiden kautta.</p>
-  <p class="p16"><strong><center>Sivuston lähdekoodi <a href="https://github.com/kaikkitietokoneista/kaikkitietokoneista">Githubissa</a></center></strong></p>
-</footer>
-<script src="src/prism.js"></script>
